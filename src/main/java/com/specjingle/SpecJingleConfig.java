ff26195ee22105cfae5d493855cc9ba520c9570e
Jorge <thepurplelefant1@gmail.com>
2024-11-17 01:06:35 +0100

    Added instruction section

--- src/main/java/com/specjingle/SpecJingleConfig.java
+++ src/main/java/com/specjingle/SpecJingleConfig.java
@@ -4,6 +4,7 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 import net.runelite.client.config.Range;
+import net.runelite.client.config.ConfigSection;
 
 @ConfigGroup("specjingle")
 public interface SpecJingleConfig extends Config
@@ -41,5 +42,30 @@ public interface SpecJingleConfig extends Config
 		return "75:4,100:0";
 	}
 
+	@ConfigSection(
+			name = "Instructions",
+			description = "Instructions for adding thresholds",
+			position = 4,
+			closedByDefault = true
+	)
+	String instructionsSection = "instructions";
+
+	@ConfigItem(
+			position = 5,
+			keyName = "thresholdInstructions",
+			name = "Entry list formatting:",
+			description = "Formatting for correct trheshold value lists",
+			section = instructionsSection
+	)
+	default String tagLegend() {
+		return "### Valid Style Format:\n\n" +
+				"Values must be added in a comma separated list. \n" +
+				"Entries must be of form spec_value:sound_value.\n" +
+				"Spec_value must be between 0 and 100 inclusive. \n"+
+				"Sound id must be value between 1 and 9 inclusive. \n"+
+				"Entries that only contain spec_value will default the sound id to 1.\n\n"+
+				"Consider the list 10:1,20:2,50:1\n\n"+
+				"This will play sound one when spec value of 10 is reached, sound 2 when spec value of 20 is reached, and sound 1 when spec value of 50 is reached.";
+	}
 
 }
