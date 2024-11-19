5d96c7584ecef39b53635a81819ee42c671c5e18
Jorge <thepurplelefant1@gmail.com>
2024-11-19 01:19:37 +0100

    "Added sound descriptions"

--- src/main/java/com/specjingle/SpecJingleConfig.java
+++ src/main/java/com/specjingle/SpecJingleConfig.java
@@ -54,18 +54,29 @@ public interface SpecJingleConfig extends Config
 			position = 5,
 			keyName = "thresholdInstructions",
 			name = "Entry list formatting:",
-			description = "Formatting for correct trheshold value lists",
+			description = "Formatting for correct threshold value lists",
 			section = instructionsSection
 	)
-	default String tagLegend() {
+	default String thresholdInstructions() {
 		return "### Valid Style Format:\n\n" +
-				"Values must be added in a comma separated list. \n" +
+				"Valuees must be added in a comma separated list. \n" +
 				"Entries must be of form spec_value:sound_value.\n" +
 				"Spec_value must be between 0 and 100 inclusive. \n"+
 				"Sound id must be value between 1 and 9 inclusive. \n"+
 				"Entries that only contain spec_value will default the sound id to 1.\n\n"+
 				"Consider the list 10:1,20:2,50:1\n\n"+
-				"This will play sound one when spec value of 10 is reached, sound 2 when spec value of 20 is reached, and sound 1 when spec value of 50 is reached.";
+				"This will play sound one when spec value of 10 is reached, sound 2 when spec value of 20 is reached, and sound 1 when spec value of 50 is reached.\n"+
+				"Sound id Names with corresponding effect values: \n"+
+				"1 is ge_collect_coins=3924\n"+
+				"2 is high_alchemy=97\n"+
+				"3 is altar_crystal_chime_=4046\n"+
+				"4 is sote_bell_high_c=4152\n"+
+				"5 is enchant_sapphire_ring=147\n"+
+				"6 is teleportblock_cast=202\n"+
+				"7 is teleportblock_impact=203\n"+
+				"8 is poh_teleport=984\n"+
+				"9 is charge_earth_orb=115\n";
+
 	}
 
 }
